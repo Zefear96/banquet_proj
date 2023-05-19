9e2950e56766d1e72c9bc5fefe96197fd2dafc1d
Miroslav987 <mirsichkar@gmail.com>
2023-05-19 22:19:16 +0300

    newTwo

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,40 +1,42 @@
 export type StorageGetItem = <T = any>(key: string) => Promise<T | null>;
 
-export const storageGetItem: StorageGetItem = async (key) => {
-	try {
-		if (typeof window === "undefined") return null;
-		const str = localStorage.getItem(key);
-		if (!str) return null;
+export const storageGetItem: StorageGetItem = async key => {
+  try {
+    console.log(window);
 
-		const parsedValue = JSON.parse(str);
-		return parsedValue;
-	} catch (error) {
-		return null;
-	}
+    if (typeof window === "undefined") return null;
+    const str = localStorage.getItem(key);
+    if (!str) return null;
+
+    const parsedValue = JSON.parse(str);
+    return parsedValue;
+  } catch (error) {
+    return null;
+  }
 };
 
 export type StorageSetItem = <T = any>(
-	key: string,
-	value: T,
+  key: string,
+  value: T
 ) => Promise<void> | void;
 
 export const storageSetItem: StorageSetItem = async (key, value) => {
-	try {
-		if (typeof window === "undefined") return;
-		const stringValue = JSON.stringify(value);
-		localStorage.setItem(key, stringValue);
-	} catch (error) {
-		return;
-	}
+  try {
+    if (typeof window === "undefined") return;
+    const stringValue = JSON.stringify(value);
+    localStorage.setItem(key, stringValue);
+  } catch (error) {
+    return;
+  }
 };
 
 export type StorageRemoveItem = <T = any>(key: string) => Promise<void>;
 
-export const storageRemoveItem: StorageRemoveItem = async (key) => {
-	try {
-		if (typeof window === "undefined") return;
-		localStorage.removeItem(key);
-	} catch (error) {
-		return;
-	}
+export const storageRemoveItem: StorageRemoveItem = async key => {
+  try {
+    if (typeof window === "undefined") return;
+    localStorage.removeItem(key);
+  } catch (error) {
+    return;
+  }
 };
